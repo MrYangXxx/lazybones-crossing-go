639de1ad13971131385d9350093c6c6eb9a3a616
yx <yx@qq.com>
2020-09-06 16:06:47 +0800

    增加record模块发布，查询，修改等方法

--- controller/record.go
+++ controller/record.go
@@ -1,9 +1,14 @@
 package controller
 
 import (
+	"github.com/juju/errors"
 	"hidevops.io/hiboot/pkg/app"
 	"hidevops.io/hiboot/pkg/at"
+	"hidevops.io/hiboot/pkg/model"
+	"lazybones-crossing-go/entity"
 	"lazybones-crossing-go/service"
+	"log"
+	"time"
 )
 
 // RestController
@@ -20,4 +25,94 @@ func newRecordController(recordService service.RecordService) *recordController 
 	return &recordController{
 		recordService: recordService,
 	}
+}
+
+//发布拖延记录
+func (c *recordController) Post(request *entity.Record) (model.Response, error) {
+	response := new(model.BaseResponse)
+
+	hasEmpty := request.UserId == "" || request.BeginTime.String() == "" || request.EndTime.String() == ""
+	if hasEmpty {
+		return response, errors.BadRequestf("请填写账号")
+	}
+
+	record := &entity.Record{
+		UserId:      request.UserId,
+		BeginTime:   request.BeginTime,
+		EndTime:     request.EndTime,
+		PublishTime: time.Now(),
+		Content:     request.Content,
+		Egg:         0,
+		Flower:      0,
+		Complete:    false,
+	}
+
+	c.recordService.AddRecord(record)
+
+	response.SetData(record)
+	return response, nil
+}
+
+//查询某用户发布记录
+func (c *recordController) FindByUserId(_ struct {
+	at.PostMapping `value:"/user"`
+}, request *entity.Record, page *int, pageSize *int) (model.Response, error) {
+	response := new(model.BaseResponse)
+
+	if *page <= 0 {
+		*page = 1
+	}
+	if *pageSize <= 0 {
+		*pageSize = 1
+	}
+
+	records, pagination, err := c.recordService.FindByFilter(request, int64(*page), int64(*pageSize))
+	if err != nil {
+		log.Print(err)
+		return nil, errors.BadRequestf("查询用户发布记录失败")
+	}
+
+	data := make(map[string]interface{})
+	data["records"] = records
+	data["pagination"] = pagination
+	response.SetData(data)
+	return response, nil
+}
+
+//首页显示的发布记录,暂根据发布时间排序查询
+func (c *recordController) Get(page *int, pageSize *int) (model.Response, error) {
+	response := new(model.BaseResponse)
+
+	if *page <= 0 {
+		*page = 1
+	}
+	if *pageSize <= 0 {
+		*pageSize = 1
+	}
+	records, pagination, err := c.recordService.Find(int64(*page), int64(*pageSize))
+	if err != nil {
+		log.Print(err)
+		return nil, errors.BadRequestf("查询发布记录失败")
+	}
+
+	data := make(map[string]interface{})
+	data["records"] = records
+	data["pagination"] = pagination
+	response.SetData(data)
+	return response, nil
+}
+
+func (c *recordController) Modify(_ struct {
+	at.PostMapping `value:"/modify"`
+}, request *entity.Record) (response model.Response, err error) {
+	response = new(model.BaseResponse)
+
+	hasEmpty := request.Id == ""
+
+	if hasEmpty {
+		return response, errors.BadRequestf("传输数据不完整")
+	}
+
+	err = c.recordService.ModifyRecord(request)
+	return
 }
--- service/record.go
+++ service/record.go
@@ -3,16 +3,20 @@ package service
 import (
 	"context"
 	"errors"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"hidevops.io/hiboot-data/starter/mongo"
 	"hidevops.io/hiboot/pkg/app"
 	"lazybones-crossing-go/entity"
+	"lazybones-crossing-go/utils"
 	"log"
 )
 
 type RecordService interface {
-	AddRecord(record *entity.Record) error
-	FindByFilter(*entity.Record, int64, int64) (records *[]entity.Record, pagination *entity.Pagination, err error)
+	AddRecord(request *entity.Record) error
+	FindByFilter(request *entity.Record, page int64, pageSize int64) (records *[]entity.Record, pagination *entity.Pagination, err error)
+	Find(page int64, pageSize int64) (records *[]entity.Record, pagination *entity.Pagination, err error)
+	ModifyRecord(request *entity.Record) error
 }
 
 type recordServiceImpl struct {
@@ -29,16 +33,16 @@ func newRecordService(client *mongo.Client) RecordService {
 	}
 }
 
-func (r *recordServiceImpl) AddRecord(record *entity.Record) error {
-	if record == nil {
+func (r *recordServiceImpl) AddRecord(request *entity.Record) error {
+	if request == nil {
 		return errors.New("user is not allowed nil")
 	}
 	db := r.client.Database("lazybones").Collection("records")
-	_, err := db.InsertOne(context.Background(), record)
+	_, err := db.InsertOne(context.Background(), request)
 	return err
 }
 
-func (r *recordServiceImpl) FindByFilter(record *entity.Record, page int64, pageSize int64) (records *[]entity.Record, pagination *entity.Pagination, err error) {
+func (r *recordServiceImpl) FindByFilter(request *entity.Record, page int64, pageSize int64) (records *[]entity.Record, pagination *entity.Pagination, err error) {
 	records = &[]entity.Record{}
 	returnRecord := entity.Record{}
 
@@ -49,7 +53,7 @@ func (r *recordServiceImpl) FindByFilter(record *entity.Record, page int64, page
 		Skip:  &skip,
 	}
 
-	res, err := db.Find(context.Background(), record, &findOptions)
+	res, err := db.Find(context.Background(), request, &findOptions)
 	for res.Next(context.TODO()) {
 		err := res.Decode(&returnRecord)
 		if err != nil {
@@ -57,7 +61,7 @@ func (r *recordServiceImpl) FindByFilter(record *entity.Record, page int64, page
 		}
 		*records = append(*records, returnRecord)
 	}
-	count, err := db.CountDocuments(context.Background(), record)
+	count, err := db.CountDocuments(context.Background(), request)
 	pagination = &entity.Pagination{
 		Page:     int(page),
 		PageSize: int(pageSize),
@@ -65,4 +69,50 @@ func (r *recordServiceImpl) FindByFilter(record *entity.Record, page int64, page
 	}
 
 	return
+}
+
+func (r *recordServiceImpl) Find(page int64, pageSize int64) (records *[]entity.Record, pagination *entity.Pagination, err error) {
+	records = &[]entity.Record{}
+	returnRecord := entity.Record{}
+
+	db := r.client.Database("lazybones").Collection("records")
+	skip := (page - 1) * pageSize
+	findOptions := &options.FindOptions{
+		Limit: &pageSize,
+		Skip:  &skip,
+		Sort:  bson.D{{"_id", -1}},
+	}
+
+	filter := &entity.Record{}
+
+	res, err := db.Find(context.Background(), filter, findOptions)
+	for res.Next(context.TODO()) {
+		err := res.Decode(&returnRecord)
+		if err != nil {
+			log.Print(err)
+		}
+		*records = append(*records, returnRecord)
+	}
+	count, err := db.CountDocuments(context.Background(), filter)
+	pagination = &entity.Pagination{
+		Page:     int(page),
+		PageSize: int(pageSize),
+		Count:    int(count),
+	}
+
+	return
+}
+
+func (r *recordServiceImpl) ModifyRecord(request *entity.Record) error {
+	if request.Id == "" {
+		return errors.New("id must not nil")
+	}
+
+	id := utils.ToMongoDBId(request.Id)
+	//id置为空,配合entity的omitempty查询时忽略空值，不为空的其他字段将更新
+	request.Id = ""
+
+	db := r.client.Database("lazybones").Collection("records")
+	_, err := db.UpdateOne(context.Background(), bson.D{{"_id", id}}, bson.D{{"$set", request}})
+	return err
 }
