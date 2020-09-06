564eb928bc379e7dcf5068210e923ba4bc3cb345
yx <yx@qq.com>
2020-09-06 16:04:42 +0800

    完善字段注释

--- entity/pagination.go
+++ entity/pagination.go
@@ -4,7 +4,7 @@ import "hidevops.io/hiboot/pkg/model"
 
 type Pagination struct {
 	model.RequestBody
-	Count    int `json:"count"`
-	Page     int `json:"page" `
-	PageSize int `json:"pageSize" `
+	Count    int `json:"count"`     //总记录数
+	Page     int `json:"page" `     //页数
+	PageSize int `json:"pageSize" ` //没页显示数
 }
