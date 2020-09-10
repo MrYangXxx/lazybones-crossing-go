8ea6a99ef9f2f96762363f516050ddd4ed4ffa2e
yx <yx@qq.com>
2020-09-10 22:44:04 +0800

    增加用户头像，用户姓名字段

--- entity/record.go
+++ entity/record.go
@@ -9,6 +9,8 @@ type Record struct {
 	model.RequestBody
 	Id          string    `json:"id" bson:"_id,omitempty"`
 	UserId      string    `json:"userId" bson:"userId,omitempty"`           //发布人Id
+	UserName    string    `json:"userName" bson:"-"`                        //发布人,-为不保存于数据库
+	UserAvatar  string    `json:"userAvatar" bson:"-"`                      //头像路径
 	BeginTime   time.Time `json:"beginTime" bson:"beginTime,omitempty"`     //拖延事件开始时间
 	EndTime     time.Time `json:"endTime" bson:"endTime,omitempty"`         //拖延事件结束时间
 	Content     string    `json:"content" bson:"content,omitempty"`         //发布内容
