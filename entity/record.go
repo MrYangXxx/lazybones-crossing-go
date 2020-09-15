697401cd43ef95dfd4e2681da36d34b496599f74
yx <yx@qq.com>
2020-09-15 17:19:22 +0800

    不省略鸡蛋鲜花数零值

--- entity/record.go
+++ entity/record.go
@@ -15,7 +15,7 @@ type Record struct {
 	EndTime     time.Time   `json:"endTime" bson:"endTime,omitempty"`         //拖延事件结束时间
 	Content     string      `json:"content" bson:"content,omitempty"`         //发布内容
 	PublishTime time.Time   `json:"publishTime" bson:"publishTime,omitempty"` //发布时间
-	Egg         int         `json:"egg" bson:"egg,omitempty"`                 //本记录获得蛋数(观众认为完成不了)
-	Flower      int         `json:"flower" bson:"flower,omitempty"`           //本记录获得花数(观众认为能够完成)
+	Egg         int         `json:"egg"`                                      //本记录获得蛋数(观众认为完成不了)
+	Flower      int         `json:"flower"`                                   //本记录获得花数(观众认为能够完成)
 	Complete    bool        `json:"complete"`                                 //结束时间到达后此拖延事件是否完成
 }
--- entity/user.go
+++ entity/user.go
@@ -15,7 +15,7 @@ type User struct {
 	BirthDay time.Time `json:"birthDay" bson:"birthDay,omitempty"` //生日，暂冗余
 	Gender   uint      `json:"gender" bson:"gender,omitempty"`     //性别，暂冗余
 	Salt     string    `json:"-" bson:"salt,omitempty"`            //密码加密盐值
-	Egg      int       `json:"egg" bson:"egg,omitempty"`           //总获得蛋数
-	Flower   int       `json:"flower" bson:"flower,omitempty"`     //总获得花数
+	Egg      int       `json:"egg"`                                //总获得蛋数
+	Flower   int       `json:"flower"`                             //总获得花数
 	Avatar   string    `json:"avatar" bson:"avatar,omitempty"`     //头像路径
 }
